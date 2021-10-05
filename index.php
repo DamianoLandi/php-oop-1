fcd91f43ab0a341ff570f829d678da2f5a4b5280
DamianoLandi <damianolandi01@gmail.com>
2021-10-05 16:46:44 +0200

    Construct

--- index.php
+++ index.php
@@ -5,7 +5,16 @@ class Movie{
     public $year;
     public $director;
     public $length;
+
+    public function __construct($_title, $_year, $_director, $_length){
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->director = $_director;
+        $this->length = $_length;
+    }
 };
+
+
 
 
 ?>
